9c2cd5c281838152418cdf58a14120f67643889a
Aiden <huyujia415@gmail.com>
2016-07-12 23:12:17 -0700

    added description

--- BinaryTreeLevelOrderTraversalII.java
+++ BinaryTreeLevelOrderTraversalII.java
@@ -1,3 +1,19 @@
+/*Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its bottom-up level order traversal as:
+[
+  [15,7],
+  [9,20],
+  [3]
+]*/
+
 /**
  * Definition for binary tree
  * public class TreeNode {
@@ -7,21 +23,20 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-import java.util.*;
 public class Solution {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        List<List<Integer>> r = new LinkedList<List<Integer>>();
-        ArrayDeque<TreeNode> q = new ArrayDeque<TreeNode>();
-        if(root==null) return r;
-        q.offer(root);
-        while(!q.isEmpty()) {
+        List<List<Integer>> r = new ArrayList<List<Integer>>();
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        if (root == null) return r;
+        q.add(root);
+        while (!q.isEmpty()) {
             int len = q.size();
-            ArrayList<Integer> level = new ArrayList<Integer>();
-            for(int i=0; i<len; i++) {
+            List<Integer> level = new ArrayList<Integer>();
+            for (int i = 0; i < len; i++) {
                 TreeNode n = q.poll();
                 level.add(n.val);
-                if(n.left!=null) q.offer(n.left);
-                if(n.right!=null) q.offer(n.right);
+                if (n.left != null) q.add(n.left);
+                if (n.right != null) q.add(n.right);
             }
             r.add(0, level);
         }
