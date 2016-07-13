45f113a613fb9754458cab354d71f1b0ae0f9ba2
Aiden <huyujia415@gmail.com>
2016-07-12 22:37:55 -0700

    simplified use of queue

--- BinaryTreeLevelOrderTraversal.java
+++ BinaryTreeLevelOrderTraversal.java
@@ -1,3 +1,19 @@
+/*Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]*/
+
 /**
  * Definition for binary tree
  * public class TreeNode {
@@ -7,23 +23,21 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
- 
-import java.util.*;
 
 public class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> r = new ArrayList<List<Integer>>();
-        ArrayDeque<TreeNode> q = new ArrayDeque<TreeNode>();
-        if(root==null) return r;
-        q.offer(root);
-        while(!q.isEmpty()) {
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        if (root == null) return r;
+        q.add(root);
+        while (!q.isEmpty()) {
             int len = q.size();
             List<Integer> level = new ArrayList<Integer>();
-            for(int i=0;i<len;i++) {
+            for (int i = 0; i < len; i++) {
                 TreeNode n = q.poll();
                 level.add(n.val);
-                if(n.left!=null) q.offer(n.left);
-                if(n.right!=null) q.offer(n.right);
+                if (n.left != null) q.add(n.left);
+                if (n.right != null) q.add(n.right);
             }
             r.add(level);
         }
