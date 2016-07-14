3b71843f544654bea5f00e482b8783cbf624e0b9
Aiden <huyujia415@gmail.com>
2016-07-13 22:03:05 -0700

    update description

--- BinaryTreePreorderTraversal.java
+++ BinaryTreePreorderTraversal.java
@@ -1,3 +1,14 @@
+/*Given a binary tree, return the preorder traversal of its nodes' values.
+
+For example:
+Given binary tree {1,#,2,3},
+   1
+    \
+     2
+    /
+   3
+return [1,2,3].*/
+
 /**
  * Definition for binary tree
  * public class TreeNode {
@@ -7,19 +18,21 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
 public class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-        //stack
-        List<Integer> r = new ArrayList<Integer>();
-        ArrayDeque<TreeNode> stack = new ArrayDeque<TreeNode>();
-        if(root==null) return r;
-        stack.push(root);
-        while(!stack.isEmpty()) {
-            TreeNode n = stack.pop();
-            r.add(n.val);
-            if(n.right!=null) stack.push(n.right);
-            if(n.left!=null) stack.push(n.left);
-        }
-        return r;
+    public List<Integer> preorderTraversal(TreeNode node) {
+	    List<Integer> list = new LinkedList<Integer>();
+	    Stack<TreeNode> rights = new Stack<TreeNode>();
+	    while(node != null) {
+		    list.add(node.val);
+		    if (node.right != null) {
+			    rights.push(node.right);
+		    }
+		    node = node.left;
+		    if (node == null && !rights.isEmpty()) {
+			    node = rights.pop();
+		    }
+	    }
+        return list;
     }
 }
