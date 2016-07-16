ad202eb2f53af1a01bc47827fdf8689207c2174f
Aiden <huyujia415@gmail.com>
2016-07-15 22:58:14 -0700

    candy sweetner

--- Candy.java
+++ Candy.java
@@ -1,21 +1,30 @@
+/*There are N children standing in a line. Each child is assigned a rating value.
+
+You are giving candies to these children subjected to the following requirements:
+
+Each child must have at least one candy.
+Children with a higher rating get more candies than their neighbors.
+What is the minimum candies you must give?*/
+
 public class Solution {
     public int candy(int[] ratings) {
-        int size = ratings.length;
-        int sum = size; //give 1 candy to each child first
-        int range = 0;
-        for (int i = 0; i < size - 1; ) {
-            int start = i;
-            while (i < size - 1 && ratings[i] < ratings[i + 1]) i++; // /
-            range = i - start;
-            sum += (range * (range + 1)) / 2;
-            
-            start = i;
-            while (i < size - 1 && ratings[i] > ratings[i + 1]) i++; // \
-            int k = i - start - 1;
-            sum += (k * (k + 1)) / 2;
-            
-            if (i - start > range) sum += i - start - range;
-            if (i < size - 1 && ratings[i] == ratings[i + 1]) i++;
+        int len = ratings.length;
+        int[] candies = new int[len];
+        Arrays.fill(candies, 1);
+
+        for (int i = 1; i < len; i++) {
+            if (ratings[i] > ratings[i - 1])
+                candies[i] = candies[i - 1] + 1;
+        }
+
+        for (int i = len - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1])
+                candies[i] = Math.max(candies[i], (candies[i + 1] + 1));
+        }
+
+        int sum = 0;
+        for (int candy: candies) {
+            sum += candy;
         }
         return sum;
     }
