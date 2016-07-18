3fbeb5b43c841598c2e03426e02ad250624dfe63
Aiden <huyujia415@gmail.com>
2016-07-17 20:04:34 -0700

    code update

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -1,18 +1,23 @@
+/*You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?*/
+
 public class Solution {
     public int climbStairs(int n) {
-        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
-        return climb(n, map);
-    }
-    private int climb(int n, HashMap<Integer,Integer> map) {
-        if(map.containsKey(n)) return map.get(n);
-        if(n<=1) return 1;
-        if(n==2) return 2;
-        int r2 = climb(n-2, map);
-        map.put(n-2,r2);
-        int r1 = climb(n-1, map);
-        map.put(n-1,r1);
-        int r = r1+r2;
-        map.put(n, r1+r2);
-        return r;
+        // base cases
+        if (n <= 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int one_step_before = 2;
+        int two_steps_before = 1;
+        int all_ways = 0;
+
+        for (int i = 2; i < n; i++) {
+            all_ways = one_step_before + two_steps_before;
+            two_steps_before = one_step_before;
+            one_step_before = all_ways;
+        }
+        return all_ways;
     }
 }
