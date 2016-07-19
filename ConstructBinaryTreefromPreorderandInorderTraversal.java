fdde6052f2456a7d744f99e1c06f9d71a88fb8a3
Aiden <huyujia415@gmail.com>
2016-07-18 22:09:13 -0700

    format

--- ConstructBinaryTreefromPreorderandInorderTraversal.java
+++ ConstructBinaryTreefromPreorderandInorderTraversal.java
@@ -1,3 +1,8 @@
+/*Given preorder and inorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.*/
+
 /**
  * Definition for binary tree
  * public class TreeNode {
@@ -7,19 +12,21 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
 public class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         return buildTree0(preorder, 0, preorder.length, inorder, 0, inorder.length);
     }
+  
     private TreeNode buildTree0(int[] preorder, int ipre, int jpre, int[] inorder, int iin, int jin) {
-        if(jpre <= ipre || jin <= iin) return null;
+        if (jpre <= ipre || jin <= iin) return null;
         TreeNode root = new TreeNode(preorder[ipre]);
-        int i=iin;
-        for(; i<jin; i++) {
-            if(inorder[i] == preorder[ipre]) break;
+        int i = iin;
+        for (; i < jin; i++) {
+            if (inorder[i] == preorder[ipre]) break;
         }
-        root.left = buildTree0(preorder, ipre+1, ipre+1+i-iin, inorder, iin, i);
-        root.right = buildTree0(preorder, ipre+1+i-iin, jpre, inorder, i+1, jin);
+        root.left = buildTree0(preorder, ipre + 1, ipre + 1 + i - iin, inorder, iin, i);
+        root.right = buildTree0(preorder, ipre + 1 + i - iin, jpre, inorder, i + 1, jin);
         return root;
     }
     // T(N)=O(N)+2T(N/2) ==> time O(nlogn)
