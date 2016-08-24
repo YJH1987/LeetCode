ebd4d07229a2c8beddbd43796520ec571e63d1c1
Aiden <huyujia415@gmail.com>
2016-08-23 23:34:18 -0700

    update

--- EvaluateReversePolishNotation.java
+++ EvaluateReversePolishNotation.java
@@ -1,37 +1,26 @@
 // Solution 1
 public class Solution {
-    interface Operator {
-        int eval(int x, int y);
-    }
-
-    private static final Map<String, Operator> OPERATORS = 
-        new HashMap<String, Operator>() {{
-            put("+", new Operator() {
-                public int eval(int x, int y) { return x + y; }
-            });
-            put("-", new Operator() {
-                public int eval(int x, int y) { return x - y; }
-            });
-            put("*", new Operator() {
-                public int eval(int x, int y) { return x * y; }
-            });
-            put("/", new Operator() {
-                public int eval(int x, int y) { return x / y; }
-            });
-        }};
-        
     public int evalRPN(String[] tokens) {
-        Stack<Integer> stack = new Stack<>();
-        for (String token : tokens) {
-            if (OPERATORS.containsKey(token)) {
-                int y = stack.pop();
-                int x = stack.pop();
-                stack.push(OPERATORS.get(token).eval(x, y));
+        int a, b;
+        Stack <Integer> S = new Stack<Integer>();
+        for (String s: tokens) {
+            if (s.equals("+")) {
+                S.add(S.pop() + S.pop());
+            } else if (s.equals("/")) {
+                b = S.pop();
+                a = S.pop();
+                S.add(a / b);
+            } else if (s.equals("*")) {
+                S.add(S.pop() * S.pop());
+            } else if (s.equals("-")) {
+                b = S.pop();
+                a = S.pop();
+                S.add(a - b);
             } else {
-                stack.push(Integer.parseInt(token));
+                S.add(Integer.parseInt(s));
             }
         }
-        return stack.pop();
+        return S.pop();
     }
 }
 
